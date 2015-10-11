ca4ac7aa89d3bfc07305882d0e6d02b05e843a6c
pumpi <webmaster@pumpi-online.de>
2015-10-11 15:12:21 +0200

    rewrite code for new data and add function to restore list over URL

--- app/build/control/build.js
+++ app/build/control/build.js
@@ -1,43 +1,55 @@
 ﻿'use strict';
 
 // Wildcard Filter for Army List builder
-armyBuilder.filter('wildcardArmy', function() {
-    return function( items, search ) {
-		var filtered = [];
-		  
-		if ( typeof search !== 'undefined' ) {
-			var searchRegEx = search.replace(/[^A-za-zÄÖÜäöü?]/g, '.*?');
-			var thisRegex = new RegExp('.*?' + searchRegEx + '.*?', 'i');
-		}
-		  
-		angular.forEach(items, function(item) {
-			if( typeof search !== 'undefined' ) {
-			  if ( thisRegex.test(item.name) ) {
-				filtered.push(item);
-			  }
-			} else {
-			  filtered.push(item);
-			}
-		});
-		  
-		return filtered;
-	};
+armyBuilder.filter('wildcardArmy', function () {
+    return function (items, search) {
+        var filtered = [];
+
+        if (typeof search !== 'undefined') {
+            var searchRegEx = search.replace(/[^A-za-zÄÖÜäöü?]/g, '.*?');
+            var thisRegex = new RegExp('.*?' + searchRegEx + '.*?', 'i');
+        }
+
+        angular.forEach(items, function (item) {
+            if (typeof search !== 'undefined') {
+                if (thisRegex.test(item.name)) {
+                    filtered.push(item);
+                }
+            } else {
+                filtered.push(item);
+            }
+        });
+
+        return filtered;
+    };
 });
 
 // Controller to build the examplelist and the top Navigation
 armyBuilder.controller('buildCtrl',
-    function ($scope, $http, $routeParams) {
+    function ($scope, $http, $routeParams, $location) {
     
     	$http.get('./data/' + $routeParams.army + '.json').
 		success(
             function(data, status, headers, config) {
-                $scope.data 			= data;
-                $scope.selectedModels 	= [];
-                $scope.gameCaster		= 1;
-                $scope.gamePoints		= 15;
-                $scope.points			= 0;
-                $scope.dropModel        = {};
-                $scope.casterPoints     = 0;
+                //clean data
+                angular.forEach(data.groups, function (item, key) {
+                    if (item.entries.length === 0) {
+                        data.groups.splice(key, 1);
+                    }
+                });
+
+                $scope.data = data.groups;
+                $scope.tiers = data.tiers;
+                $scope.selectedModels = [];
+                $scope.gameCaster = 1;
+                $scope.gamePoints = 15;
+                $scope.gameTier = 0;
+                $scope.points = 0;
+                $scope.dropModel = {};
+                $scope.casterPoints = 0;
+
+                //restore from URL
+                $scope.restoreSearch();
 
                 var favicon = new Favico();
                 var image = $('#' + $routeParams.army)[0];
@@ -55,28 +67,15 @@ armyBuilder.controller('buildCtrl',
                 alert('error reading ' + $routeParams.army + '.json');
 		    }
         );
-		
-		// Sticky Container
-		$(document).ready( function() {
-			$('.sticky').each( function() {
-				var stickyPos = $(this).offset();
-				var stickyWidth = $(this).outerWidth();
-				var sticky = $(this);
 
-				$(window).scroll( function() {
-					if ( $(this).scrollTop() > stickyPos.top + 9.5 ) {
-						sticky.css({position: 'fixed', top: 0, left: (stickyPos.left - 7.5), width: stickyWidth, zIndex: 9999});
-					} else {
-						sticky.removeAttr('style');
-					}
-				});			
-			});
+        $scope.accordion = function(id) {
+            var $this = $('.'+id);
+            $this.slideDown();
+            $this.parent().siblings().find('.accordion-container').slideUp();
+        };
 
-
-		});
-		
-		// Check if this model aviable
-		$scope.checkModelAviable = function(model, type) {
+		// Check if this model available
+		$scope.checkModelAvailable = function(model) {
 			// gameCaster not set or no usable int
 			if ( typeof $scope.gameCaster === 'undefined' || $scope.gameCaster.length === 0 || isNaN($scope.gameCaster)) {
                 return true;
@@ -88,38 +87,38 @@ armyBuilder.controller('buildCtrl',
             }
 			
 			// Warlock have max value in selectedModels
-            if ( /warlock/i.test(type) && $scope.countType('warlock') >= $scope.gameCaster ) {
+            if ( /warlock/i.test(model.type) && $scope.countType('warlock') >= $scope.gameCaster ) {
 				return true;
             }
             
             // Warcaster have max value in selectedModels
-            if ( /warcaster/i.test(type) && $scope.countType('warcaster') >= $scope.gameCaster ) {
+            if ( /warcaster/i.test(model.type) && $scope.countType('warcaster') >= $scope.gameCaster ) {
 				return true;
             }
 			
 			// No Caster in selectedModels we can not select an Warbeast or Warjack
-            if ( /warbeast|warjack/i.test(type) && $scope.countType('warlock|warcaster') === 0 ) {
+            if ( /warbeast|warjack/i.test(model.type) && $scope.countType('warlock|warcaster') === 0 ) {
 				return true;
             }
             
             // The Points to use are higher as the available points but check if warbeast an we have available caster points
-            if ( /warbeast|warjack/i.test(type) && parseInt($scope.casterPoints) > 0) {
-		    	if ( ( parseInt($scope.gamePoints) - parseInt($scope.points) + parseInt($scope.casterPoints) ) < parseInt(model.mkiicost)) {
+            if ( /warbeast|warjack/i.test(model.type) && parseInt($scope.casterPoints) > 0) {
+		    	if ( ( parseInt($scope.gamePoints) - parseInt($scope.points) + parseInt($scope.casterPoints) ) < parseInt(model.cost)) {
 		            return true;
 				}
-            } else if ( !/warcaster|warlock/i.test(type) ) {
-                if ( ( parseInt($scope.gamePoints) - parseInt($scope.points) ) < parseInt(model.mkiicost)) {
+            } else if ( !/warcaster|warlock/i.test(model.type) ) {
+                if ( ( parseInt($scope.gamePoints) - parseInt($scope.points) ) < parseInt(model.cost)) {
                     return true;
                 }
             }
-            
+
             // Check if field allowance at cap
-            if ( !model.basicFA || parseInt(model.basicFA) < 100 ) {
+            if ( !model.fa || model.fa !== 'U' ) {
             	var mc = 0;
             	var fa = false;
             	$.each($scope.selectedModels, function(key, selectedModel) {
                     // if Character and we always have in list
-                    if ( !model.basicFA && selectedModel.name === model.name ) {
+                    if ( model.fa === 'C' && selectedModel.name === model.name ) {
                         fa = true;
                         return false;
                     }
@@ -130,7 +129,7 @@ armyBuilder.controller('buildCtrl',
                     }
 
                     // if field allowance model full
-                    if ( model.basicFA <= mc ) {
+                    if ( model.fa <= mc ) {
                         fa = true;
                         return false;
                     }
@@ -143,9 +142,9 @@ armyBuilder.controller('buildCtrl',
             }
             
             // An unit or weapon attachment but not set the base model
-            if ( model.hasOwnProperty('baseUnit') ) {
+            if ( model.hasOwnProperty('restricted_to') ) {
             	for (var i = 0, len = $scope.selectedModels.length; i < len; i++) {
-					if ( $scope.selectedModels[i].name === model.baseUnit || model.baseUnit === '*' ) {
+					if ( $scope.selectedModels[i].id === model.restricted_to || model.restricted_to === '*' ) {
 						return false;
 					}
 				}				
@@ -159,18 +158,17 @@ armyBuilder.controller('buildCtrl',
         $scope.countType = function(type) {
             var count = 0;
             var matcher = new RegExp(type, "i");
-            $.each( $scope.selectedModels, function( key, selectedModel ) {
-                if ( matcher.test(selectedModel.type) ) {
-                    count ++;
+            angular.forEach($scope.selectedModels, function (selectedModel) {
+                if (matcher.test(selectedModel.type)) {
+                    count++;
                 }
             });
             return count;
         };
 
         $scope.dropCallback = function(event, ui) {
-            var type = ui.draggable.data().type;
             var dragScope = angular.element(ui.draggable).scope();
-            $scope.addModel(dragScope.model, type);
+            $scope.addModel(dragScope.model);
         };
 
         $scope.startCallback = function(event, ui) {
@@ -179,23 +177,22 @@ armyBuilder.controller('buildCtrl',
         };
 
         // Add an model from the left to the right
-        $scope.addModel = function(model, type) {
+        $scope.addModel = function(model) {
         	var copy = angular.copy(model);
-                copy.type = type;
 
             // If type warbeast or warjack we must add it after the last warbeast/warjack oder after the last warlock/warcaster
             // If baseUnit set we must add this model to an unit
             var findIndex = false;
-            if ( /warbeast|warjack/i.test(type) ) {
+            if ( /warbeast|warjack/i.test(model.type) ) {
                 for (var i = $scope.selectedModels.length -1; i >= 0; i--) {
                     if ( /warbeast|warlock|warjack|warcaster/i.test($scope.selectedModels[i].type) ) {
                         findIndex = i;
                         break;
                     }
                 }
-            } else if ( model.hasOwnProperty('baseUnit') ) {
+            } else if ( model.hasOwnProperty('restricted_to') ) {
                 for (var i = $scope.selectedModels.length -1; i >= 0; i--) {
-                    if ( $scope.selectedModels[i].name === model.baseUnit ) {
+                    if ( $scope.selectedModels[i].name === model.restricted_to ) {
                         findIndex = i;
                         break;
                     }
@@ -246,7 +243,7 @@ armyBuilder.controller('buildCtrl',
         
         // Is there enough points to use max size
         $scope.canUseMax = function(model) {
-       		return ( !model.useMax && ( parseInt($scope.gamePoints) - parseInt($scope.points) + parseInt(model.mkiicost) ) < parseInt(model.mkiiexpand) );
+       		return ( !model.useMax && ( parseInt($scope.gamePoints) - parseInt($scope.points) + parseInt(model.cost) ) < parseInt(model.costMax) );
         };
         
         // Calculate the available Points
@@ -256,20 +253,20 @@ armyBuilder.controller('buildCtrl',
 
 			$.each( $scope.selectedModels, function( key, model ) {
 				if ( /warlock|warcaster/i.test(model.type) ) {
-                    casterPoints = casterPoints + parseInt(model.mkiicost);
+                    casterPoints = casterPoints + parseInt(model.cost);
 				} else if ( /warjack|warbeast/i.test(model.type) ) {
                     // if he bonTo none we must calculate the points to the sumPoints and not to casterPoints
-                    if ( model.hasOwnProperty('bondTo') && model.bondTo === 'none' ) {
-                        sumPoints = sumPoints + parseInt(model.mkiicost);
+                    if ( model.hasOwnProperty('bondTo') && model.restricted_to === 'none' ) {
+                        sumPoints = sumPoints + parseInt(model.cost);
                     } else {
-                        casterPoints = casterPoints - parseInt(model.mkiicost);
+                        casterPoints = casterPoints - parseInt(model.cost);
                     }
 				} else {
                     // Must we use the base cost of model or the max
                     if ( model.useMax ) {
-                        sumPoints = sumPoints + parseInt(model.mkiiexpand);
+                        sumPoints = sumPoints + parseInt(model.costMax);
                     } else {
-                        sumPoints = sumPoints + parseInt(model.mkiicost);
+                        sumPoints = sumPoints + parseInt(model.cost);
                     }
 				}
 			});
@@ -282,6 +279,9 @@ armyBuilder.controller('buildCtrl',
 
             // Set the available Caster points for later Checks
             $scope.casterPoints = casterPoints;
+
+            // Store the models in URL
+            $scope.updateSearch();
 		};
         
         // No sort for ng-repeat
@@ -292,5 +292,89 @@ armyBuilder.controller('buildCtrl',
 		    return Object.keys(obj);
 		};
 
+        $scope.updateSearch = function() {
+            //get the selectedModels as string
+            var search = {},
+                sel = [];
+            console.log($scope.selectedModels);
+            angular.forEach($scope.selectedModels, function(model) {
+                var modStr = model.id;
+
+                //an unit have an max size
+                if ( model.hasOwnProperty('useMax') && model.useMax === true ) {
+                    modStr += ':useMax';
+                }
+
+                //a bonded model
+                if ( model.hasOwnProperty('bonded') && model.bonded === 1 ) {
+                    modStr += ':bonded';
+                }
+                sel.push( modStr );
+            });
+            search.sel = btoa(sel);
+            search.caster = $scope.gameCaster;
+            search.points = $scope.gamePoints;
+            search.tier = $scope.gameTier;
+
+            $location.search( search );
+
+            console.log(atob($location.search().sel));
+        };
+
+        $scope.restoreSearch = function() {
+            var search = $location.search();
+            // restore gamePoints
+            if (search.points) {
+                $scope.gamePoints = search.points;
+            }
+            //restore gameCaster
+            if (search.caster) {
+                $scope.gameCaster = search.caster;
+            }
+            // restore gameTier
+            if (search.tier) {
+                $scope.gameTier = search.tier;
+            }
+            //restore selectedModels
+            if (search.sel) {
+                var decode = atob(search.sel),
+                    sel = decode.split(',');
+                angular.forEach(sel, function(val) {
+                    $scope.addModelByString(val);
+                });
+            }
+        };
+
+        $scope.addModelByString = function(string) {
+            console.log('addModelByString');
+            //split by id an option
+            var args = string.split(':');
+
+            //search in data for id = args[0]
+            var add = {};
+            angular.forEach($scope.data, function(grp) {
+                angular.forEach(grp.entries, function(entrie) {
+                    if ( entrie.id === args[0] ) {
+                        add = angular.copy(entrie);
+                        return false;
+                    }
+                });
+                if ( add.length > 0 ) {
+                    return false;
+                }
+            });
+
+            if (!$.isEmptyObject(add) ) {
+                if ( args[1] === 'bonded' ) {
+                    add.bonded = 1;
+                }
+
+                if ( args[1] === 'useMax' ) {
+                    add.useMax = true;
+                }
+                $scope.selectedModels.push(add);
+                $scope.calculateAvailablePoints();
+            }
+        };
     }
 );
