8560ccb6f5717a6ace01a1224758f235ee859da4
pumpi <webmaster@pumpi-online.de>
2016-01-10 12:21:07 +0100

    test for google analytics push

--- app.js
+++ app.js
@@ -45,9 +45,11 @@ armyBuilder.config(['$routeProvider',
 // Google Analytics send
 armyBuilder.run(function($rootScope, $location, $window){
     $rootScope.$on('$routeChangeSuccess', function() {
-        if (!$window.ga || /127\.0\.0\.1.*?/i.test($location.host())) {
+        if (!$window.ga || /127\.0\.0\.1/i.test($location.host())) {
+            console.log('dev');
             return;
         }
+        console.log('push analytics');
         $window.ga('send', 'pageview', { page: $location.path() });
     });
 });
